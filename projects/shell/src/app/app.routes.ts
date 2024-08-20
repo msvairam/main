e0ddfdbb6b56a7d129791d05c9f1825fe96991fd
msvairam <mutvairam@gmail.com>
2024-08-20 22:48:02 +0530

    mf tools

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,16 +1,32 @@
 import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation';
+import { WebComponentWrapper, WebComponentWrapperOptions } from '@angular-architects/module-federation-tools';
 
-export const routes: Routes = [{
-    path: '', loadComponent: () => import('./app.component').then(m => m.AppComponent),
-    pathMatch: 'full'
-},
-{
-    path: 'flight', loadComponent: () => 
-        loadRemoteModule({
-            type: "module",
-            remoteEntry:  'http://localhost:4201/remoteEntry.js',
-            exposedModule:  './Component'
-        }).then(m => m.flightComponent)
-}
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./app.component').then((m) => m.AppComponent),
+    pathMatch: 'full',
+  },
+  {
+    path: 'flight',
+    loadComponent: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Component',
+      }).then((m) => m.flightComponent),
+  },
+  {
+    path: 'react',
+    component: WebComponentWrapper,
+    data: {
+      type: 'script',
+      remoteEntry:
+        'https://witty-wave-0a695f710.azurestaticapps.net/remoteEntry.js',
+      remoteName: 'react',
+      exposedModule: './web-components',
+      elementName: 'react-element',
+    } as WebComponentWrapperOptions,
+  },
 ];
