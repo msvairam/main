f4fad20ee6109cf785b68982caa0b4253b58a8bf
msvairam <mutvairam@gmail.com>
2024-08-20 21:40:28 +0530

    shared_lib

--- projects/mfe1/src/app/flight/flight.component.ts
+++ projects/mfe1/src/app/flight/flight.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { SharedLibService } from 'shared-lib';
 
 @Component({
   selector: 'app-flight',
   standalone: true,
-  imports: [],
   templateUrl: './flight.component.html',
   styleUrl: './flight.component.css'
 })
 export class flightComponent {
+  constructor(public shared: SharedLibService) {
+
+  }
+
+  ngOnInit() {
+    console.log(this.shared.userName);
+  }
+  changeName() {
+    this.shared.userName = 'Miliran';
+  }
 
 }
--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedLibService } from 'shared-lib';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'shell';
+  constructor(public shared: SharedLibService) {
+
+  }
+  ngAfterViewInit() {
+    this.shared.userName = 'vairamuthu';
+  }
 }
